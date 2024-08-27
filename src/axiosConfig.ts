59a410e791e56b93a81ac0619fa37161bf745fb9
TR7J <167967143+TR7J@users.noreply.github.com>
2024-08-27 20:38:16 +0300

    ...

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000", // adjust baseURL as needed
+  baseURL: "https://construction-site-back.onrender.com", // adjust baseURL as needed
 });
 
 // Add a request interceptor
